a5f023c923bff0b1791ea17fd9ab92deb7f44b27
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2024-02-04 23:12:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+async function fetchData(){
+	const data= await fetch("https://jsonplaceholder.typicode.com/todos/1");
+	const res= await data.json();
+	console.log(res);
+}
+fetchData()
